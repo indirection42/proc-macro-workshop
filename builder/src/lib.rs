f65c87fa22afdcc23d94193991043b21ab21e9f1
indirection42 <jiyuanz95@gmail.com>
2024-06-20 20:18:15 +0800

    feat(builder): 06 finished

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Field, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -13,8 +13,17 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         |f| {
             let name = &f.ident;
             let ty = &f.ty;
+            if let Type::Path(type_path) = ty {
+                if let Some(segment) = type_path.path.segments.first() {
+                    if segment.ident == "Option" {
+                        return quote! {
+                            #name: #ty
+                        };
+                    }
+                }
+            }
             quote! {
-                #name: Option<#ty>
+                #name: std::option::Option<#ty>
             }
         },
         true,
@@ -25,7 +34,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         |f| {
             let name = &f.ident;
             quote! {
-                #name: None
+                #name: std::option::Option::None
             }
         },
         true,
@@ -36,9 +45,29 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         |f| {
             let name = &f.ident;
             let ty = &f.ty;
+            if let Type::Path(type_path) = ty {
+                if let Some(segment) = type_path.path.segments.first() {
+                    if segment.ident == "Option" {
+                        if let syn::PathArguments::AngleBracketed(angle_bracketed_args) =
+                            &segment.arguments
+                        {
+                            if let Some(syn::GenericArgument::Type(inner_ty)) =
+                                angle_bracketed_args.args.first()
+                            {
+                                return quote! {
+                                    pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                                        self.#name = std::option::Option::Some(#name);
+                                        self
+                                    }
+                                };
+                            }
+                        }
+                    }
+                }
+            }
             quote! {
                 pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                    self.#name = Some(#name);
+                    self.#name = std::option::Option::Some(#name);
                     self
                 }
             }
@@ -50,6 +79,15 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         &input.data,
         |f| {
             let name = &f.ident;
+            if let Type::Path(type_path) = &f.ty {
+                if let Some(segment) = type_path.path.segments.first() {
+                    if segment.ident == "Option" {
+                        return quote! {
+                            #name: self.#name.clone()
+                        };
+                    }
+                }
+            }
             quote! {
                 #name: self.#name.clone().ok_or("Field not set")?
             }
@@ -70,8 +108,8 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
         impl #builder_ident {
             #builder_setters
-            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
-                Ok(#name {
+            pub fn build(&mut self) -> std::result::Result<#name, std::boxed::Box<dyn std::error::Error>> {
+                std::result::Result::Ok(#name {
                     #builder_build
                 })
             }

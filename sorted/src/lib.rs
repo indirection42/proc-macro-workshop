aaf440f1277a2c00320495113483a190c15e1df0
indirection42 <jiyuanz95@gmail.com>
2024-06-21 10:49:43 +0800

    feat(sorted): 02 finished

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,24 @@
-use proc_macro::TokenStream;
+use proc_macro2::Span;
+use syn::{Error, Item};
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as Item);
 
-    unimplemented!()
+    match input {
+        Item::Enum(_) => {}
+        _ => {
+            return Error::new(Span::call_site(), "expected enum or match expression")
+                .to_compile_error()
+                .into();
+        }
+    }
+
+    let output = quote::quote! {
+        #input
+    };
+    proc_macro::TokenStream::from(output)
 }
